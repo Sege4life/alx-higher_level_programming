bf03a0ca42c163ded81819fd8d54bb141591f303
Sege4life <folayemi.oluwasegun@gmail.com>
2022-12-01 06:20:54 -0800

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
-if _name_== '_main_':
+if __name__ == '__main__':
     res = 0
     cnt = 1
 
     while cnt < len(argv):
-        res += int(argv{cnt})
+        res += int(argv[cnt])
         cnt += 1
-        print(res)
+    print(res)
