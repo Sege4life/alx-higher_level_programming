5c922677990ac462a0cafa28a7e07a73e5a515ee
Sege4life <folayemi.oluwasegun@gmail.com>
2022-12-01 06:17:17 -0800

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if _name_== '_main_':
+    res = 0
+    cnt = 1
+
+    while cnt < len(argv):
+        res += int(argv{cnt})
+        cnt += 1
+        print(res)
